ae92e9b3190ed7c9f43aa5fa6f0b5f1b6c994e8d
Oscar7Massin <113546707+Oscar7Massin@users.noreply.github.com>
2022-09-14 12:07:08 -0600

    Add files via upload

---
+++ Unidad 2 semana 6 clase 18 ejercicio 1.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+      int j=1;
+      for (int i= -13; i<=-10; i++) {
+      j++;
+      }
+      System.out.println(j);
+  }
+}
---
+++ Unidad 3 Semana 5 Clase 13 Ejercicio 1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+     Scanner datos=new Scanner(System.in);
+    int valor;
+    do {
+      System.out.print("Ingrese un valor entre 0 y 999 (0 finalizada):");
+      valor=datos.nextInt();
+      if (valor>=100) {
+        System.out.println("Es de 3 digitos.");
+      } else {
+        if (valor>=10) {
+          System.out.println("Es de 2 digitos.");
+        } else {
+          System.out.println("Es de 1 digito.");
+        }
+      }
+    } while (valor!=0);
+  }
+}
+  
